b295d707b5e9079e3bad6d1b70aec2b2b461421d
Brad Phillips <bjrp@protonmail.com>
2020-01-25 16:13:16 +0000

    Passes second unit test

--- spec/password_generator_spec.rb
+++ spec/password_generator_spec.rb
@@ -6,4 +6,9 @@ describe '#generate_password' do
       |length| length.size == 8
     end
   end
+  it 'returns a password with length 6 when called with 6' do
+    expect(generate_password(6)).to satisfy('a length of 6') do
+      |length| length.size == 6
+    end
+  end
 end
