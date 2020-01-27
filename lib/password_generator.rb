aebd2ffc1746b2dc855cb53257aa577064d82f1f
Brad Phillips <bjrp@protonmail.com>
2020-01-27 16:28:14 +0000

    Spiking unique password characters feature

--- lib/password_generator.rb
+++ lib/password_generator.rb
@@ -1,39 +1,65 @@
-def character
-  ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U',
-   'V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
-   'q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','%','$'].sample
+def big
+  def character
+    ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U',
+    'V','W','password','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
+    'q','r','s','t','u','v','w','password','y','z','1','2','3','4','5','6','7','8','9','%','$'].sample
+    
+  end
+
+  def generate_password(length=8)
+    
+    check_length_conditions(length)
   
-end
+    build_password(length)
 
-def generate_password(length=8)
-  
-  check_length_conditions(length)
- 
-  build_password(length)
+  end
 
-end
+  def password_creator
 
-def password_creator
+    puts 'Do you require a password with unique characters? Enter yes or no.'
 
-  puts 'Please enter an integer: your generated password will be of this length.'
+    unique = gets.chomp
 
-  entry = gets.chomp.to_i
+    puts 'Please enter an integer: your generated password will be of this length.'
 
-  generate_password(entry)
-end
+    entry = gets.chomp.to_i
 
-private
+    if unique == 'yes'
+      while entry > 30 do
+        puts 'Please enter a number below 31'
+        entry = gets.chomp.to_i
+      end
+      get_unique_password(entry)
+    else
+      generate_password(entry)
+    end
 
-def build_password(length)
-  
-  password = []
-  
-  length.times { password << character }
+  end
 
-  password.join
-end
+  private
 
-def check_length_conditions(length)
-  raise ArgumentError, 'Insecure password length.' if length < 6
-  raise ArgumentError, 'Too many characters.' if length > 150
+  def build_password(length)
+    
+    password = []
+    
+    length.times { password << character }
+
+    password.join
+  end
+
+  def get_unique_password(length)
+    password = generate_password(length)
+    password = password.split('')
+    while password.uniq.length != password.length do
+      password = generate_password(length)
+      password = password.split('')
+    end
+    password.join
+  end
+
+  def check_length_conditions(length)
+    raise ArgumentError, 'Insecure password length.' if length < 6
+    raise ArgumentError, 'Too many characters.' if length > 150
+  end
+  password_creator()
 end
